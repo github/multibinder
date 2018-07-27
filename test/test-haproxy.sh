9e69bed36bfb3fb5809c2bf5cda5e64cffa9f8db
Theo Julienne <theojulienne@github.com>
2018-07-27 16:05:42 +1000

    Fix haproxy wrapper to keep FDs around until exec.

--- test/test-haproxy.sh
+++ test/test-haproxy.sh
@@ -27,3 +27,25 @@ begin_test "haproxy runs with multibinder"
   curl --max-time 5 http://localhost:$(offset_port $TEST_PORT)/ | grep -q 'Request forbidden'
 )
 end_test
+
+begin_test "haproxy can bind to large numbers of file descriptors"
+(
+  setup
+
+  export TEMPDIR
+
+  echo >$TEMPDIR/haproxy-many-fds.cfg.erb
+
+  for i in $(seq 1000); do
+    echo "  bind <%= bind_tcp('127.0.0.1', $((TEST_PORT + $i))) %>" >>$TEMPDIR/haproxy-many-fds.cfg.erb
+  done
+
+  bundle exec env MULTIBINDER_SOCK=${TEMPDIR}/multibinder.sock ruby bin/multibinder-haproxy-erb -f $TEMPDIR/haproxy-many-fds.cfg.erb --erb-write-only
+
+  if grep 'bind fd@' $TEMPDIR/haproxy-many-fds.cfg | sort | uniq -d | grep -q 'bind fd@'; then
+  	echo 'Expected unique FDs, but found duplicates:'
+  	cat $TEMPDIR/haproxy-many-fds.cfg
+  	exit 1
+  fi
+)
+end_test
